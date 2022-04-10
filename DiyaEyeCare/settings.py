e72324d1ea610ec4346ade4eb6d0331c618a8857
Amol Khedekar <amlu9421@gmail.com>
2022-04-10 11:48:32 +0530

    CHANGE : Updated media settings

--- DiyaEyeCare/settings.py
+++ DiyaEyeCare/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
